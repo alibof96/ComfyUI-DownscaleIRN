3cfaafab010f1db771dbbf0f2c59a83dec1c5908
alibof96 <alibof96@gmail.com>
2025-10-22 16:40:00 +0200

    Create __init__.py file for package initialization

---
+++ __init__.py
@@ -0,0 +1,4 @@
+from .comfyui_downscale_irn import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+
+# If anything fails during import, ComfyUI will log it. This file ensures the
+# folder is a Python package so ComfyUI can import it as a custom node module.
